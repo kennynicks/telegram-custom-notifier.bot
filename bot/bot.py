3ee64421fd34854b4f0b32393b5f4d1964106a7e
Leon Ochsenfeld <ochsenfeld@hotmail.com>
2021-06-28 15:28:07 +0200

    feat: added status command

--- bot/bot.py
+++ bot/bot.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 import logging
 import os
+from typing import List
 
-from telegram import ParseMode
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-    Updater
+    Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 )
+
+from scraper.scraper import Scraper
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -15,12 +18,36 @@ logger = logging.getLogger(__name__)
 
 
 class Bot:
-    def __init__(self) -> None:
+    def __init__(self, scraper: Scraper) -> None:
         self.updater = Updater(os.getenv("BOT_TOKEN"))
+        self.scraper = scraper
 
-    def send_message(self, chat_message: str) -> None:
-        self.updater.bot.sendMessage(os.getenv("CHAT_ID"), chat_message, parse_mode=ParseMode.MARKDOWN_V2)
+    def send_message(self, chat_message: str, parse_mode: ParseMode = ParseMode.MARKDOWN_V2) -> None:
+        self.updater.bot.sendMessage(os.getenv("CHAT_ID"), chat_message, parse_mode=parse_mode)
+
+    def status(self, update: Update, context: CallbackContext) -> None:
+        print(self.scraper.get_status())
+        self.send_message(self.scraper.get_status())
+
+    def button_clicked(self, update: Update, context: CallbackContext) -> None:
+        query = update.callback_query
+        query.answer()
+        switcher = {"status": lambda: self.status(update, context)}
+        switcher.get(query.data)()
+
+    def start(self, update: Update, context: CallbackContext) -> None:
+        """Sends a message with 5 inline buttons attached."""
+        update.message.reply_text(text="Menu:",
+                                  reply_markup=self.build_keyboard())
+
+    def build_keyboard(self) -> InlineKeyboardMarkup:
+        buttons: List[InlineKeyboardButton] = [InlineKeyboardButton("Status", callback_data="status")]
+        return InlineKeyboardMarkup.from_column(buttons)
 
     def run(self) -> None:
+        dispatcher = self.updater.dispatcher
+        dispatcher.add_handler(CommandHandler("start", self.start))
+        dispatcher.add_handler(CallbackQueryHandler(self.button_clicked))
+        dispatcher.add_handler(CommandHandler("status", self.status))
         self.updater.start_polling()
         self.updater.idle()
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -21,12 +21,14 @@ class Scraper(ArticleSubscriber):
         self.bike_discount.add_subscriber(self)
         self.bike_24.add_subscriber(self)
         self.amazon.add_subscriber(self)
+        self.articles: List[BaseArticle] = []
 
     def on_change(self, available: bool, shop: Shop, article: BaseArticle) -> None:
         for subscriber in self.subscribers:
             subscriber.on_change(available, shop, article)
 
     def watch_article(self, article: BaseArticle):
+        self.articles.append(article)
         if Shop.BIKE_COMPONENTS in article.article_entries:
             self.bike_components.watch(article)
         if Shop.BIKE_DISCOUNT in article.article_entries:
@@ -35,6 +37,20 @@ class Scraper(ArticleSubscriber):
             self.bike_24.watch(article)
         if Shop.AMAZON in article.article_entries:
             self.amazon.watch(article)
+
+    def get_status(self) -> str:
+        status_message = ""
+        for idx, article in enumerate(self.articles):
+            status_message += "{}\.    {}\n".format(idx + 1, article.name)
+            amazon_state = self.amazon.get_status(article)
+            bike24_state = self.bike_24.get_status(article)
+            bikecomponents_state = self.bike_components.get_status(article)
+            bikediscount_state = self.bike_discount.get_status(article)
+            states = [x for x in [amazon_state, bike24_state, bikecomponents_state, bikediscount_state] if
+                      x is not None]
+            status_message += "\n".join(states)
+            status_message += "\n\n"
+        return status_message
 
     def add_subscriber(self, subscriber: ArticleSubscriber):
         self.subscribers.append(subscriber)
